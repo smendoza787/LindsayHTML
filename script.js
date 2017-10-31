2c5f6b4d19d3f046c6e4faf45ad4b26fc371414b
Sergio Mendoza <smendoza787@gmail.com>
2017-10-30 22:02:25 -0700

    converted logo to link, styled landing page, add jquery navbar effect

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
 $(function() {
+  // navbar title dissappears above 275px
+  $(document).scroll(function(e) {
+    var $scroll = $(this).scrollTop()
+    var $scrolledName = $(".scrolled-name")
+
+    if ($scroll > 175) {
+      $scrolledName.removeClass('hidden');
+      $scrolledName.removeClass('visually-hidden');
+      setTimeout(function() {
+        $scrolledName.removeClass('visually-hidden');
+      }, 20);
+    } else {
+      $scrolledName.addClass('visually-hidden');
+      $scrolledName.one('transitionend', function(e) {
+        $scrolledName.addClass('visually-hidden');
+      });
+    }
+  })
+
   $('a[href*="#"]:not([href="#"])').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
       var target = $(this.hash);
